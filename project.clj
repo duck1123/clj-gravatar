c5e2f664f216ea493a57a98ad334ee4740d56d33
Daniel E. Renfer <duck@kronkltd.net>
2011-06-27 07:45:29 -0400

    kronkltd

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-gravatar "0.0.1"
+(defproject net.kronklt/clj-gravatar "0.0.1"
   :description "gravatar api wrapper for clojure"
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  [org.clojure/data.json "0.1.0"]])
