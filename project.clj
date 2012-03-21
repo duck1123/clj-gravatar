7ec8b701bcc45fba20df506ceb38918458549b43
Daniel E. Renfer <duck@kronkltd.net>
2012-03-21 01:35:30 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.kronkltd/clj-gravatar "0.0.1"
+(defproject net.kronkltd/clj-gravatar "0.1.0-SNAPSHOT"
   :description "gravatar api wrapper for clojure"
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
-                 [org.clojure/data.json "0.1.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.2"]])
