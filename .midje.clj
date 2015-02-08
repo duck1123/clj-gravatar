b01016c95e75128f4daaac47f3dc17e034075c35
Daniel E. Renfer <duck@kronkltd.net>
2015-02-08 17:33:15 -0500

    use junit output

--- .midje.clj
+++ .midje.clj
@@ -1,3 +1,3 @@
-(change-defaults :emitter 'midje.emission.plugins.tap
+(change-defaults :emitter 'midje.emission.plugins.junit
                  :print-level :print-facts
                  :colorize false)
